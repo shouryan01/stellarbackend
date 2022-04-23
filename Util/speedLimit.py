715805cc31e396a836e8cdf4c4e242fb39e16618
Muhaddatha <mzabdulg@umich.edu>
2022-04-23 14:18:29 -0400

    package speedLimit information

--- Util/speedLimit.py
+++ Util/speedLimit.py
@@ -3,9 +3,6 @@ import json, requests
 
 
 
-# seconds = 4
-
-
 base_url = "https://api.stellantis-developers.com"
 headers = {
     "x-api-key": API_KEY, "user": API_USERNAME, "password": API_PASSWORD
@@ -53,7 +50,7 @@ def main():
     set_bearer_token(bearer_token)
    
     while True:
-        print("tick")
+        print("-----------------")
         time.sleep(1.0 - ((time.time() - start_time) % 1.0))
         data = json.dumps(get_last_known_data(VEHICLE_VIN))
         data_2 = json.loads(data)
@@ -62,18 +59,12 @@ def main():
         for item in data_2["Items"]:
             vin = item["vin"]
             if (item["signal"] == "Speedometer"):
-                # print(type(item["value"]))
                 speed = item["value"]
-                # print(item["value"])
             
             if (item["signal"] == "Latitude"):
-                # print(type(item["value"]))
                 latitude = item["value"]
-                # print(item["value"])
             
             if (item["signal"] == "Longitude"):
-                # print(type(item["value"]))
-                # print(item["value"])
                 longitude = item["value"]
 
         x = requests.get("https://dev.virtualearth.net/REST/v1/Routes/SnapToRoad?points={},{}&includeTruckSpeedLimit=true&IncludeSpeedLimit=true&speedUnit=MPH&travelMode=driving&key=ApB3pE4UR5Px4wJangIVFcsZGmLzxlHvntQeam933MqxSW4aqIme9SaZO1T_XECy".format(latitude, longitude))
@@ -86,9 +77,14 @@ def main():
                     speedLimit = z["speedLimit"]
 
 
-        speedInformationList = [vin, speed, speedLimit]
-        speedinformation = json.dumps(speedInformationList)
+        speedInformationDict = {
+            "vin": vin,
+            "speed": float(speed),
+            "speedLimit": float(speedLimit),
+        }
+        speedInformation = json.dumps(speedInformationDict)
 
+        print(speedInformation)
 
 if __name__ == "__main__":
     main()
